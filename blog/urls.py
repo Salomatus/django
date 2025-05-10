79974c509e0386aaafabd4899ff9c1dc3870614d
Oleg <logardyt@gmail.com>
2025-05-11 00:04:15 +0700

    other templates and blogs at

--- blog/urls.py
+++ blog/urls.py
@@ -15,5 +15,7 @@ urlpatterns = [
     path("blog/create/", BlogCreateView.as_view(), name="blogs_create"),
     path("blog/<int:pk>/", BlogDetailView.as_view(), name="blogs_detail"),
     path("blog/<int:pk>/update", BlogUpdateView.as_view(), name="blogs_update"),
+
     path("blog/<int:pk>/delete", BlogDeleteView.as_view(), name="blogs_delete"),
+
 ]
